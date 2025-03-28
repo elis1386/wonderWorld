2352894ff37d2a5d4353166957f41b82e4d19b90
elis1386 <elis1386@gmail.com>
2025-03-28 12:59:52 +0200

    fix karma file

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,6 @@
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
     basePath: "",
@@ -10,11 +13,16 @@ module.exports = function (config) {
       require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      jasmine: {},
-      clearContext: false,
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
-      suppressAll: true,
+      suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/library"),
@@ -22,10 +30,10 @@ module.exports = function (config) {
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
     reporters: ["progress", "kjhtml"],
-    browsers: ["ChromeHeadless", "Chrome_no_sandbox"],
+    browsers: ["Chrome", "Chrome_no_sandbox"],
     customLaunchers: {
       Chrome_no_sandbox: {
-        base: "ChromeHeadless",
+        base: "Chrome",
         flags: ["--no-sandbox", "--headless", "--disable-gpu"],
       },
     },
