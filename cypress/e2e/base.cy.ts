523c25a03ca1b07d68d6f2aacb813753181fc7c3
elis1386 <elis1386@gmail.com>
2025-04-28 22:03:31 +0300

    add cypress tests

---
+++ cypress/e2e/base.cy.ts
@@ -0,0 +1,101 @@
+/// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
+
+describe("User Registration and Flow", () => {
+  const randomPassword = faker.internet.password();
+  const randomEmail = faker.internet.email();
+  const randomName = faker.internet.userName();
+  const randomSurname = faker.person.lastName();
+  const randomUrl = faker.internet.url();
+
+  it("should sign up, sign in, logout and interact with book", () => {
+    cy.visit("https://wonderworld-2a0e3.web.app/");
+
+    // Click to Sign Up
+    cy.get(".sign-up").click();
+
+    //Fillout the registration form
+    cy.get(":nth-child(2) > .form-control").type(randomName); // email
+    cy.get(":nth-child(3) > .form-control").type(randomSurname); // password
+    cy.get(":nth-child(4) > .form-control").type(randomEmail);
+    cy.get(":nth-child(5) > .form-control").type(randomPassword);
+    cy.get(":nth-child(6) > .form-control").type(randomUrl);
+
+    cy.get(".w-100").click();
+    cy.url().should("include", "/user");
+
+    // logout
+    cy.get(".d-block").click();
+    cy.get(":nth-child(3) > .dropdown-item").click();
+    cy.url().should("include", "/");
+  });
+});
+describe("User Sign in", () => {
+  const userPassword = "Alexia1234";
+  const userEmail = "alexia@aalto.com";
+
+  it("sign in, logout and interact with book", () => {
+    cy.visit("https://wonderworld-2a0e3.web.app/");
+
+    // Click to Sign Up
+    cy.get(".login").click();
+
+    //Fillout the registration form
+    cy.get(":nth-child(2) > .form-control").type(userEmail); // email
+    cy.get(":nth-child(3) > .form-control").type(userPassword); // password
+
+    cy.get(".w-100").click();
+    cy.url().should("include", "/user");
+
+    cy.wait(2000);
+
+    // logout
+    cy.get(".d-block").click();
+    cy.get(":nth-child(3) > .dropdown-item").click();
+    cy.url().should("include", "/");
+  });
+});
+describe("Borrow book", () => {
+  const userPassword = "Alexia1234";
+  const userEmail = "alexia@aalto.com";
+
+  it("sign in and borrow book", () => {
+    cy.visit("https://wonderworld-2a0e3.web.app/");
+
+    // Click to Sign Up
+    cy.get(".login").click();
+
+    //Fillout the registration form
+    cy.get(":nth-child(2) > .form-control").type(userEmail); // email
+    cy.get(":nth-child(3) > .form-control").type(userPassword); // password
+
+    cy.get(".w-100").click();
+    cy.url().should("include", "/user");
+
+    cy.get(".logo").click();
+    cy.wait(2000);
+    cy.get("app-new-arrive.ng-tns-c11-2 > .container > .card-box > :nth-child(1) > .info > .book-name").click();
+    cy.get(".btn").contains("Borrow this book").click();
+    cy.get(".btn").contains("Borrow this book").should("have.class", "disabled");
+  });
+});
+describe("Return book", () => {
+  const userPassword = "Alexia1234";
+  const userEmail = "alexia@aalto.com";
+
+  it("sign in and borrow book", () => {
+    cy.visit("https://wonderworld-2a0e3.web.app/");
+
+    // Click to Sign Up
+    cy.get(".login").click();
+
+    //Fillout the registration form
+    cy.get(":nth-child(2) > .form-control").type(userEmail); // email
+    cy.get(":nth-child(3) > .form-control").type(userPassword); // password
+
+    cy.get(".w-100").click();
+    cy.url().should("include", "/user");
+    cy.get(".btn").contains("Return Book").click();
+    cy.contains("You have no books yet. Please go to the main page and add books.").should("be.visible");
+  });
+});
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
-import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
+import { test, expect } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 
 const randomPassword = faker.internet.password();
 const randomEmail = faker.internet.email();
@@ -8,64 +8,58 @@ const randomSurname = faker.person.lastName();
 const randomUrl = faker.internet.url();
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://wonderworld-2a0e3.firebaseapp.com/');
+  await page.goto("https://wonderworld-2a0e3.firebaseapp.com/");
 });
 
-test('sign up ', async ({ page }) => {
-  page.getByRole('button', { name: 'Sign-up' }).click();
-  await page.getByPlaceholder('enter you first name').fill(randomName);
-  await page.keyboard.press('Tab');
-  await page.getByPlaceholder('enter you last name').fill(randomSurname);
-  await page.keyboard.press('Tab');
-  await page.getByPlaceholder('name@example.com').fill(randomEmail);
-  await page.keyboard.press('Tab');
-  await page.getByPlaceholder('Password').fill(randomPassword);
-  await page.keyboard.press('Tab');
-  await page.getByPlaceholder('add your photo').fill(randomUrl);
-  await page.keyboard.press('Tab');
-  await page.getByRole('button', { name: 'Sign up' }).click();
+test("sign up ", async ({ page }) => {
+  page.getByRole("button", { name: "Sign-up" }).click();
+  await page.getByPlaceholder("enter you first name").fill(randomName);
+  await page.keyboard.press("Tab");
+  await page.getByPlaceholder("enter you last name").fill(randomSurname);
+  await page.keyboard.press("Tab");
+  await page.getByPlaceholder("name@example.com").fill(randomEmail);
+  await page.keyboard.press("Tab");
+  await page.getByPlaceholder("Password").fill(randomPassword);
+  await page.keyboard.press("Tab");
+  await page.getByPlaceholder("add your photo").fill(randomUrl);
+  await page.keyboard.press("Tab");
+  await page.getByRole("button", { name: "Sign up" }).click();
   await expect(page).toHaveURL(/user/);
 });
 
-test('sign in as admin and sign out', async ({ page }) => {
+test("sign in as admin and sign out", async ({ page }) => {
   await page.pause();
 
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.getByPlaceholder('name@example.com').fill('elis1386@gmail.com');
-  await page.keyboard.press('Tab');
-  await page.getByPlaceholder('Password').fill('Elis1234');
-  await page.keyboard.press('Tab');
-  await page.getByRole('main').getByRole('button', { name: 'Sign in' }).click();
-  await page.goto('https://wonderworld-2a0e3.firebaseapp.com/admin');
-  await page.locator('a').nth(1).click();
-  await page.getByText('Sign out').click();
-  await expect(page).toHaveURL('https://wonderworld-2a0e3.firebaseapp.com/');
+  await page.getByRole("button", { name: "Sign in" }).click();
+  await page.getByPlaceholder("name@example.com").fill("elis1386@gmail.com");
+  await page.keyboard.press("Tab");
+  await page.getByPlaceholder("Password").fill("Elis1234");
+  await page.keyboard.press("Tab");
+  await page.getByRole("main").getByRole("button", { name: "Sign in" }).click();
+  await page.goto("https://wonderworld-2a0e3.firebaseapp.com/admin");
+  await page.locator("a").nth(1).click();
+  await page.getByText("Sign out").click();
+  await expect(page).toHaveURL("https://wonderworld-2a0e3.firebaseapp.com/");
 });
-test('sign in as user and sign out', async ({ page }) => {
+test("sign in as user and sign out", async ({ page }) => {
   await page.pause();
 
-  await page.getByRole('button', { name: 'Sign in' }).click();
-  await page.getByPlaceholder('name@example.com').fill('anna@gmail.com');
-  await page.keyboard.press('Tab');
-  await page.getByPlaceholder('Password').fill('Anna1234');
-  await page.keyboard.press('Tab');
-  await page.getByRole('main').getByRole('button', { name: 'Sign in' }).click();
-  await page.goto('https://wonderworld-2a0e3.firebaseapp.com/user');
-  await page.locator('a').nth(1).click();
-  await page.getByText('Sign out').click();
-  await expect(page).toHaveURL('https://wonderworld-2a0e3.firebaseapp.com/');
+  await page.getByRole("button", { name: "Sign in" }).click();
+  await page.getByPlaceholder("name@example.com").fill("anna@gmail.com");
+  await page.keyboard.press("Tab");
+  await page.getByPlaceholder("Password").fill("Anna1234");
+  await page.keyboard.press("Tab");
+  await page.getByRole("main").getByRole("button", { name: "Sign in" }).click();
+  await page.goto("https://wonderworld-2a0e3.firebaseapp.com/user");
+  await page.locator("a").nth(1).click();
+  await page.getByText("Sign out").click();
+  await expect(page).toHaveURL("https://wonderworld-2a0e3.firebaseapp.com/");
 });
 
-test('Borrow book if not log in', async ({ page }) => {
-  page
-    .locator('app-new-arrive')
-    .getByRole('heading', { name: 'Neropatin päiväkirja: Karu' })
-    .click();
-  await page.getByRole('button', { name: 'Borrow this book' }).click();
-  page.locator('app-book-list').getByText('Sorry... You need to sign in');
-  page.getByRole('link', { name: 'sign in' }).click();
-  await expect(page).toHaveURL(
-    'https://wonderworld-2a0e3.firebaseapp.com/sign-in'
-  );
+test("Borrow book if not log in", async ({ page }) => {
+  page.locator("app-new-arrive").getByRole("heading", { name: "Neropatin päiväkirja: Karu" }).click();
+  await page.getByRole("button", { name: "Borrow this book" }).click();
+  page.locator("app-book-list").getByText("Sorry... You need to sign in");
+  page.getByRole("link", { name: "sign in" }).click();
+  await expect(page).toHaveURL("https://wonderworld-2a0e3.firebaseapp.com/sign-in");
 });
-
