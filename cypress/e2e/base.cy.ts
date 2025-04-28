e02bc2e3a04cc679e13b5a10600f5ed0fdc264e8
elis1386 <elis1386@gmail.com>
2025-04-28 22:37:53 +0300

    fix data attribute for tests

--- cypress/e2e/base.cy.ts
+++ cypress/e2e/base.cy.ts
@@ -15,13 +15,13 @@ describe("User Registration and Flow", () => {
     cy.get(".sign-up").click();
 
     //Fillout the registration form
-    cy.get(":nth-child(2) > .form-control").type(randomName); // email
-    cy.get(":nth-child(3) > .form-control").type(randomSurname); // password
-    cy.get(":nth-child(4) > .form-control").type(randomEmail);
-    cy.get(":nth-child(5) > .form-control").type(randomPassword);
-    cy.get(":nth-child(6) > .form-control").type(randomUrl);
+    cy.get('[data-cy="input-first-name"]').type(randomName); // email
+    cy.get('[data-cy="input-last-name"]').type(randomSurname); // password
+    cy.get('[data-cy="input-email"]').type(randomEmail);
+    cy.get('[data-cy="input-password"]').type(randomPassword);
+    cy.get('[data-cy="input-image"]').type(randomUrl);
 
-    cy.get(".w-100").click();
+    cy.get('[data-cy="submit-signup"]').click();
     cy.url().should("include", "/user");
 
     // logout
@@ -41,10 +41,10 @@ describe("User Sign in", () => {
     cy.get(".login").click();
 
     //Fillout the registration form
-    cy.get(":nth-child(2) > .form-control").type(userEmail); // email
-    cy.get(":nth-child(3) > .form-control").type(userPassword); // password
+    cy.get('[data-cy="input-login-email"]').type(userEmail); // email
+    cy.get('[data-cy="input-login-password"]').type(userPassword); // password
 
-    cy.get(".w-100").click();
+    cy.get('[data-cy="submit-login"]').click();
     cy.url().should("include", "/user");
 
     cy.wait(2000);
@@ -66,17 +66,17 @@ describe("Borrow book", () => {
     cy.get(".login").click();
 
     //Fillout the registration form
-    cy.get(":nth-child(2) > .form-control").type(userEmail); // email
-    cy.get(":nth-child(3) > .form-control").type(userPassword); // password
+    cy.get('[data-cy="input-login-email"]').type(userEmail); // email
+    cy.get('[data-cy="input-login-password"]').type(userPassword); // password
 
-    cy.get(".w-100").click();
+    cy.get('[data-cy="submit-login"]').click();
     cy.url().should("include", "/user");
 
     cy.get(".logo").click();
     cy.wait(2000);
     cy.get("app-new-arrive.ng-tns-c11-2 > .container > .card-box > :nth-child(1) > .info > .book-name").click();
-    cy.get(".btn").contains("Borrow this book").click();
-    cy.get(".btn").contains("Borrow this book").should("have.class", "disabled");
+    cy.get('[data-cy="button-borrow-book"]').click();
+    cy.get('[data-cy="button-borrow-book"]').should("have.class", "disabled");
   });
 });
 describe("Return book", () => {
